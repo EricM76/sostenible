c7ef50c12002b61fdb957178a1a7c642f79b85db
Francisco Martin <franproducciones636@gmail.com>
2024-10-20 21:09:17 -0300

    commiteo cambios en controladores

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,5 +3,29 @@ const path = require ('path')
 module.exports = {
     index : (req,res) => {
 
-        return res.render('home')},
+        return res.render('home')
+    },
+    admin : (req,res) => {
+
+        return res.render('admin')
+    },
+    contacto : (req,res) => {
+        return res.render('contacto')
+    },
+    plantando : (req,res) => {
+
+        return res.render('plantandofuturo')
+    },
+    hub : (req,res) => {
+
+        return res.render ('hubrecircular')
+    },
+    podcast : (req,res) => {
+
+        return res.render ('podcast')
+    },
+    webinar : (req,res) => {
+
+        return res.render ('webinar')
+    },
 }

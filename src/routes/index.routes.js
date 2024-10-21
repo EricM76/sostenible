b7d216cb851492e2ff5b48059eec70a1b608b7ce
Francisco Martin <franproducciones636@gmail.com>
2024-10-20 21:00:18 -0300

    corregidos metodos y enrutado

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {index} = require('../controllers/indexController')
+const {index,contacto,plantando,hub,podcast,admin,webinar} = require('../controllers/indexController')
 
 router.get('/',index)
+router.get('/contacto',contacto)
+router.get('/plantandofuturo',plantando)
+router.get('/hubrecircular',hub)
+router.get('/podcast',podcast)
+router.get('/admin',admin)
+router.get('/webinar',webinar)
 
 module.exports = router
